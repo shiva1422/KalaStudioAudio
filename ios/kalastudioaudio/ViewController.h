5e5ab0b50f32882df878522afc6caff346601f4c
rahulshah1108 <pspr142@gmail.com>
2021-06-16 00:40:01 -0400

    filebrowser

--- ios/kalastudioaudio/ViewController.h
+++ ios/kalastudioaudio/ViewController.h
@@ -7,7 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIDocumentPickerDelegate,UIDocumentInteractionControllerDelegate>
+- (IBAction)openFile:(id)sender;
 
 
 @end
--- ios/kalastudioaudio/ViewController.m
+++ ios/kalastudioaudio/ViewController.m
@@ -6,8 +6,9 @@
 //
 
 #import "ViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+@interface ViewController ()<UIDocumentPickerDelegate,UIDocumentInteractionControllerDelegate>
 
 @end
 
@@ -19,4 +20,29 @@
 }
 
 
+- (IBAction)openFile:(id)sender
+{
+    
+    
+    NSLog(@"open the file");
+    //NSString<UTType *> *types = @[[NSString alloc] initWithObjects:(UTType*)kUTTypeAudio , nil ];
+    UIDocumentPickerViewController * docPicker ;
+    @try{
+            docPicker = [[UIDocumentPickerViewController alloc] initWithDocumentTypes:@[@"public.audio"]
+                                                                               inMode:UIDocumentPickerModeImport];
+        
+       // docPicker.modalPresentationStyle = UIModalPresentationPopover;
+        [self presentViewController:docPicker animated:YES completion:nil];
+    }
+    @catch(NSException *docexc)
+    {
+        NSLog(@"DOCUMENT PICKER EXCEPTION-%@",[docexc callStackSymbols]);
+    }
+    docPicker.delegate = self;
+    
+}
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url
+{
+    NSLog(@"imported-%@",url.absoluteString);
+}
 @end
